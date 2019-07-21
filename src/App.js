21c034645be9fe8d2c1ea245698bc8c2f291801d
github username <github email address>
2019-07-20 21:47:15 -0600

    Adding basic login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import SignupPage from './app_components/Signup.js'
 import { Route, Switch, withRouter } from 'react-router-dom'
+import LoginPage from './app_components/Login.js'
 
 class App extends React.Component {
 
@@ -10,6 +11,7 @@ class App extends React.Component {
   return (
     <div className="App">
     <Route exact path='/signup' render={({history})=><SignupPage history={history}/>}/>
+    <Route exact path='/login' render={({history})=><LoginPage history={history}/>}/>
     </div>
   );
   }
---
+++ src/app_components/Login.js
@@ -0,0 +1,116 @@
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Lock from "@material-ui/icons/Lock"
+// @material-ui/icons
+import Email from "@material-ui/icons/Email";
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+
+import loginPageStyle from "assets/jss/material-kit-react/views/loginPage.jsx";
+
+//import image from "images/bg7.jpg";
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    //  use this to make the card to appear after the page has been rendered
+    this.state = {
+      cardAnimaton: "cardHidden"
+    };
+  }
+  componentDidMount() {
+    // we add a hidden class to the card and after 700 ms we delete it and the transition appears
+    setTimeout(
+      function() {
+        this.setState({ cardAnimaton: "" });
+      }.bind(this),
+      700
+    );
+  }
+  render() {
+    const { classes, ...rest } = this.props;
+    //const image = <img src={require('images/bg7.jpg')} alt='background'/>
+    return (
+      <div>
+        <div
+          className={classes.pageHeader}
+          style={{
+            //backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "top center"
+          }}
+        >
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={4}>
+                <Card className={classes[this.state.cardAnimaton]}>
+                  <form className={classes.form}>
+                    <CardHeader color="primary" className={classes.cardHeader}>
+                      <h4>Log In</h4>
+                    </CardHeader>
+                    <p className={classes.divider}></p>
+                    <CardBody>
+                      <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "email",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Email className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Password"
+                        id="pass"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "password",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Lock className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          ),
+                          autoComplete: "off"
+                        }}
+                      />
+                    </CardBody>
+                    <CardFooter className={classes.cardFooter}>
+                      <Button simple color="primary" size="lg">
+                        Submit
+                      </Button>
+                    </CardFooter>
+                  </form>
+                </Card>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(loginPageStyle)(LoginPage);
