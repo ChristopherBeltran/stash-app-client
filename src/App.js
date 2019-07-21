1350b49c7850ed1b62dacd7c0c5c6ec58d637ccf
github username <github email address>
2019-07-20 21:38:54 -0600

    Standing up a basic sign up page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignupPage from './app_components/Signup.js'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
-function App() {
+class App extends React.Component {
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Route exact path='/signup' render={({history})=><SignupPage history={history}/>}/>
     </div>
   );
+  }
 }
 
 export default App;
