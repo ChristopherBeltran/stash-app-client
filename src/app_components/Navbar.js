db5638ca1d5e07c1ce73c480a6e892baa8b757e4
github username <github email address>
2019-08-04 15:22:31 -0700

    Standing up Stash, Stream and Profile NavBar links

--- src/app_components/Navbar.js
+++ src/app_components/Navbar.js
@@ -3,13 +3,18 @@ import Header from "components/Header/Header.jsx"
 import HeaderLinks from "components/Header/HeaderLinks.jsx"
 import Button from "components/CustomButtons/Button.jsx";
 import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+
 
 const NavBar = () => {
 
         return(
             <div>
-                <Header color="info" brand="Stash App" position="sticky" >
-                
+                <Header color="info" brand="Stash App" position="sticky" 
+                rightLinks={<HeaderLinks />}>
+                <Toolbar>
+                <Button size="lg" color="transparent" link="true">Stream</Button>
+                </Toolbar>
                 </Header>
             </div>
         )
