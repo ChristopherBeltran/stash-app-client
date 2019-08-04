bc03521342ccedd80b5a9a6ace911530a0eec714
github username <github email address>
2019-08-03 20:20:25 -0700

    Standing up basic StashContainer

---
+++ src/app_components/StashContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { connect } from 'react-redux'
+import Grid from '@material-ui/core/Grid';
+import GridItem from '../components/Grid/GridItem.jsx'
+
+
+class StashContainer extends React.Component {
+
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default StashContainer;
