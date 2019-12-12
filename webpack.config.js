00dec4f1b36728d25fbb138eddb4e77852b25642
github username <christod.beltran@gmail.com>
2019-12-12 09:48:51 -0700

    json update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 //{ test: /\.(jpeg|jpg|png|gif)$/,loader: 'url-loader?limit=10240' }
 
-module.exports = {
-    module: {
-        loaders: [
-          { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
-        ]
-      },
-  };
+//module.exports = {
+//    module: {
+//        loaders: [
+//          { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+//        ]
+//      },
+//  };
