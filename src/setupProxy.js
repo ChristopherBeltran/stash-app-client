b5500890e9bd142dc86b920702e29eca9e8c3681
github username <christod.beltran@gmail.com>
2019-12-11 19:03:55 -0700

    Removing setupProxy file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware');
-module.exports = function(app) {
-    app.use(proxy('/api', 
-        { target: 'https://the-stash-app.herokuapp.com/' }
-    ));
-}
+//const proxy = require('http-proxy-middleware');
+//module.exports = function(app) {
+//    app.use(proxy('/api', 
+//        { target: 'https://the-stash-app.herokuapp.com/' }
+//    ));
+//}
