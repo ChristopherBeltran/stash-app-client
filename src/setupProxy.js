b3bffab04d6a6c86eab9eb1e3f052fb245c1b2a0
github username <christod.beltran@gmail.com>
2019-12-12 10:40:46 -0700

    Removing setupProxy file causing require error

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-//const proxy = require('http-proxy-middleware');
-//module.exports = function(app) {
-//    app.use(proxy('/api', 
-//        { target: 'https://the-stash-app.herokuapp.com/' }
-//    ));
-//}
