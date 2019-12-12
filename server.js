e9f97b38c8f6fb1b203cccd0f0329875608e7622
github username <christod.beltran@gmail.com>
2019-12-12 16:02:03 -0700

    Path fix in server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.listen(process.env.PORT || 8080);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
-    app.get('*', (req, res) => {
-      res.sendFile('index.html', { root: __dirname });
+    app.get('*', function (req, res) {
+      const index = path.join(__dirname, 'build', 'index.html');
+      res.sendFile(index);
     });
   }
