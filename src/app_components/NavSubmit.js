39ede1ffb583d0b419d79e5909b8840bd08232f1
github username <christod.beltran@gmail.com>
2020-02-11 14:09:55 -0700

    Moving fab styling from CSS to NavSubmit component in makeStyles function

--- src/app_components/NavSubmit.js
+++ src/app_components/NavSubmit.js
@@ -11,6 +11,7 @@ import ListItem from "@material-ui/core/ListItem";
 import Button from "components/CustomButtons/Button.jsx";
 import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
 import Fab from '@material-ui/core/Fab';
+import { makeStyles } from "@material-ui/core";
 
 function NavSubmit({ ...props }) {
   const { classes } = props;
@@ -22,19 +23,35 @@ function NavSubmit({ ...props }) {
     streamUpdate()
   }
 
+  const useStyles = makeStyles({
+    root: {
+      margin: '0px',
+      top: 'auto',
+      right: '127.133px',
+      bottom: '20px',
+      position: 'fixed',
+      color: 'white',
+      backgroundColor: '#9c27b0',
+      fontWeight: 'bold'
+
+    }
+  })
+
+  const fabClasses = useStyles();
+
   var submitButton = ''
   if(stream.source_ids !== null && stream.source_ids !== undefined){
     if(stream.source_ids.length > 0){
       if(isDesktop === true){
       submitButton = <Button color="primary" size="lg" block={true} type="submit">Create Stream</Button>
       } else {
-      submitButton = <Fab id="fab-button" variant="extended" type="submit">Create Stream</Fab>
+      submitButton = <Fab classes={fabClasses} variant="extended" type="submit">Create Stream</Fab>
       }
     } else {
       if(isDesktop === true){
       submitButton = <Button disabled={true} color="primary" size="lg" block={true} type="submit">Create Stream</Button>
       } else {
-      submitButton = <Fab id="fab-button-disabled" disabled={true} color="primary" variant="extended"  type="submit">Create Stream</Fab>
+      submitButton = <Fab classes={fabClasses} disabled={true} variant="extended"  type="submit">Create Stream</Fab>
       }
     }
   }
